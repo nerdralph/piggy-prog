db15d8e68db92cebfcb3860895614ee2021844a1
Nerd Ralph <ralphdoncaster@gmail.com>
2018-08-06 16:47:58 -0300

    working write signature

--- PiggyFuse/PiggyFuse.ino
+++ PiggyFuse/PiggyFuse.ino
@@ -21,23 +21,40 @@
 #define  WHFUSE  0x747C
 #define  WEFUSE  0x666E
 
-#define  RLFUSE  0x686C
-#define  RHFUSE  0x7A7E
-#define  REFUSE  0x6A6E
+const int PageSize = 32;
+
+// fuse operation codes
+enum FuseOp {
+  RLFUSE = 0x686C,
+  RHFUSE = 0x7A7E,
+  REFUSE = 0x6A6E,
+  RLOCK = 0x787C  
+};
+
+// HV programming commands
+enum HvCmd {
+  NoOp = 0x00,
+  ReadFuse = 0x04,
+  ReadSig = 0x08,
+  WriteFlash = 0x10,
+  WriteFuse = 0x40,
+  ChipErase = 0x80
+};
 
 // Define ATTiny series signatures
-#define  ATTINY13   0x9007  // L: 0x6A, H: 0xFF             8 pin
-#define  ATTINY24   0x910B  // L: 0x62, H: 0xDF, E: 0xFF   14 pin
-#define  ATTINY25   0x9108  // L: 0x62, H: 0xDF, E: 0xFF    8 pin
-#define  ATTINY44   0x9207  // L: 0x62, H: 0xDF, E: 0xFF   14 pin
-#define  ATTINY45   0x9206  // L: 0x62, H: 0xDF, E: 0xFF    8 pin
-#define  ATTINY84   0x930C  // L: 0x62, H: 0xDF, E: 0xFF   14 pin
-#define  ATTINY85   0x930B  // L: 0x62, H: 0xDF, E: 0xFF    8 pin
+#define  ATTINY13   0x9007  // L: 0x7A, H: 0xFF             8 pin
+#define  ATTINY24   0x910B  // L: 0xE2, H: 0xDF, E: 0xFF   14 pin
+#define  ATTINY25   0x9108  // L: 0xE2, H: 0xDF, E: 0xFF    8 pin
+#define  ATTINY44   0x9207  // L: 0xE2, H: 0xDF, E: 0xFF   14 pin
+#define  ATTINY45   0x9206  // L: 0xE2, H: 0xDF, E: 0xFF    8 pin
+#define  ATTINY84   0x930C  // L: 0xE2, H: 0xDF, E: 0xFF   14 pin
+#define  ATTINY85   0x930B  // L: 0xE2, H: 0xDF, E: 0xFF    8 pin
+#define  BLANK      0xFFFF  // blank signature
 
 byte success;
 
 void setup() {
-  digitalWrite(RST, HIGH);  // input pullup turns on NPN & shorts 12V 
+  digitalWrite(RST, HIGH);  // turns on NPN & shorts 12V 
   pinMode(VCC, OUTPUT);
   pinMode(GND, OUTPUT);
   pinMode(SDI, OUTPUT);
@@ -45,7 +62,7 @@ void setup() {
   pinMode(SCI, OUTPUT);
   pinMode(SDO, OUTPUT);     // Configured as input when in programming mode
   Serial.begin(57600);
-  Serial.println(F("HSVP fuse resetter"));
+  Serial.println(F("\nHSVP fuse resetter"));
 
   digitalWrite(VCC, HIGH);  // Vcc On
   delayMicroseconds(20);    // wait 20us for VCC to rise
@@ -57,15 +74,27 @@ void setup() {
   unsigned int sig = readSignature();
   Serial.print(F("Signature: "));
   Serial.println(sig, HEX);
+  Serial.print(F("OSCCAL: "));
+  Serial.println(readOsccal(), HEX);
   printFuses();
+  byte lock = readFuse(RLOCK);
+  if ( lock != 0xFF) {
+    Serial.println(F("Lock bits set.  Performing chip erase."));
+    chipErase();
+  }
+  if (sig == BLANK) {
+    Serial.println(F("Blank signature. resetting to t13"));
+    writeSig(0x60);
+  }
   success = 1;
   byte hfuse;
   if (sig == ATTINY13) {
     Serial.println(F("t13."));
-    hfuse = 0xE4;
-    // if dW & BOD are on, turn them off
+    //hfuse = 0xE4;   // SELFPRG, DW, BOD1.8V, RSTDSBL
+    hfuse = 0xE7;   // SELFPRG, DW
     if (readFuse(RHFUSE) == hfuse){
-      hfuse = 0xEE;
+      //hfuse = 0xEE;   // SELFPRG, RSTDSBL
+      hfuse = 0xFF;   // alternate fuse setting
       success = 2;
     }
     writeFuse(WLFUSE, 0x7A);
@@ -73,15 +102,19 @@ void setup() {
   } else if (sig == ATTINY24 || sig == ATTINY44 || sig == ATTINY84 ||
              sig == ATTINY25 || sig == ATTINY45 || sig == ATTINY85) {
     Serial.println(F("tX[45]."));
-    hfuse = 0x1E;
-    // if dW & BOD are on, turn them off
+    //hfuse = 0x1E;     // RSTDSBL, DW, SPIEN, BOD1.8V
+    hfuse = 0x9F;     // DW, SPIEN
     if (readFuse(RHFUSE) == hfuse){
-      hfuse = 0x5F;
+      //hfuse = 0x5F;   // RSTDSBL, SPIEN
+      hfuse = 0xDF;   // alternate fuse setting SPIEN
       success = 2;
     }
     writeFuse(WLFUSE, 0xE2);
     writeFuse(WHFUSE, hfuse);
     writeFuse(WEFUSE, 0xFE);
+  } else if (sig == BLANK) {
+    Serial.println(F("missing signature."));
+    success = 0;
   } else {
     Serial.println(F("unrecognized device."));
     success = 0;
@@ -94,6 +127,10 @@ void setup() {
     Serial.print(F("Product"));
   }
   Serial.println(F(" fuses programmed."));
+
+  sig = readSignature();
+  Serial.print(F("Signature: "));
+  Serial.println(sig, HEX);
   printFuses();
   Serial.println(F("Fuse resetter finished."));
   Serial.println(F("Reset programmer to run again."));
@@ -103,7 +140,7 @@ void loop() {
   pinMode(SDI, INPUT);
   pinMode(SII, INPUT);
   pinMode(SCI, INPUT);
-  digitalWrite(RST, HIGH);  // input pullup turns on NPN & turns off 12V 
+  digitalWrite(RST, HIGH);      // turns on NPN & shorts 12V 
 
   digitalWrite(LED, HIGH);
   if ( !success){
@@ -123,10 +160,10 @@ void loop() {
   delay(2000);
 }
 
-byte shiftOut (byte val1, byte val2) {
+byte shiftOut (byte sdata, byte sinstr) {
   int inBits = 0;
-  unsigned int dout = (unsigned int) val1 << 2;
-  unsigned int iout = (unsigned int) val2 << 2;
+  unsigned int dout = (unsigned int) sdata << 2;
+  unsigned int iout = (unsigned int) sinstr << 2;
   for (int ii = 10; ii >= 0; ii--)  {
     digitalWrite(SDI, !!(dout & (1 << ii)));
     digitalWrite(SII, !!(iout & (1 << ii)));
@@ -138,18 +175,69 @@ byte shiftOut (byte val1, byte val2) {
   return inBits >> 2;
 }
 
-void writeFuse (unsigned int fuse, byte val) {
-  shiftOut(0x40, 0x4C);
-  shiftOut( val, 0x2C);
-  shiftOut(0x00, (byte) (fuse >> 8));
-  shiftOut(0x00, (byte) fuse);
+byte sendCmd(HvCmd command) {
+  return shiftOut(command, 0x4C);
+}
+
+void loadLoAddr(byte addr) {
+  shiftOut(addr, 0x0C);
+}
+
+void busyWait(){
   while (!digitalRead(SDO));        // wait until SDO goes high
 }
 
-byte readFuse(unsigned int fuse){
-         shiftOut(0x04, 0x4C);      // read fuse
-         shiftOut(0x00, (byte) (fuse >> 8));
-  return shiftOut(0x00, (byte) fuse);
+void loadData(byte addr, int data) {
+  loadLoAddr(addr);
+  shiftOut(data, 0x2C);
+  shiftOut(data>>8, 0x3C);
+  shiftOut(0x0, 0x7D);
+  shiftOut(0x0, 0x7C);  
+}
+
+void writeSig(byte osccal)
+{
+  sendCmd(WriteFlash);
+  // osscal is stored in high byte of address 0
+  loadData(0, osccal << 8 | 0x1E);
+  // t13 signature = 0x9007
+  loadData(1, 0xFF90);
+  loadData(2, 0xFF07);
+  // undocumented sig page program command
+  shiftOut(0x93, 0x55);
+  shiftOut(0x0, 0x45);
+  shiftOut(0x0, 0x35);
+  busyWait();
+  sendCmd(NoOp);
+}
+
+void loadPage(byte buf[PageSize]) {
+  sendCmd(WriteFlash);
+  // for (byte addr = 0; addr
+}
+
+void chipErase()
+{
+  sendCmd(ChipErase);
+  shiftOut(0x00, 0x64);
+  shiftOut(0x00, 0x6C);
+  busyWait();
+  sendCmd(NoOp);
+}
+
+void writeFuse (unsigned int fuse, byte val) {
+  sendCmd(WriteFuse);
+  shiftOut( val, 0x2C);
+  shiftOut(0x00, (byte) (fuse >> 8));
+  shiftOut(0x00, (byte) fuse);
+  busyWait();
+}
+
+byte readFuse(FuseOp fusecmd){
+  sendCmd(ReadFuse);
+  shiftOut(0x00, (byte) (fusecmd >> 8));
+  //return shiftOut(0x00, (byte) fusecmd);
+  return sendCmd(NoOp);
 }
 
 void printFuses () {
@@ -162,18 +250,29 @@ void printFuses () {
   Serial.print(val, HEX);
   val = readFuse(REFUSE);
   Serial.print(F(", EFuse: "));
+  Serial.print(val, HEX);
+  val = readFuse(RLOCK);
+  Serial.print(F(", Lock: "));
   Serial.println(val, HEX);
+}
+
+byte readOsccal() {
+  sendCmd(ReadSig);
+  loadLoAddr(0);
+  shiftOut(0x00, 0x78);
+  return sendCmd(NoOp);
 }
 
 unsigned int readSignature () {
   unsigned int sig = 0;
   byte val;
   for (int ii = 1; ii < 3; ii++) {
-          shiftOut(0x08, 0x4C);
-          shiftOut(  ii, 0x0C);
+          sendCmd(ReadSig);
+          loadLoAddr(ii);
           shiftOut(0x00, 0x68);
     val = shiftOut(0x00, 0x6C);
     sig = (sig << 8) + val;
   }
   return sig;
 }
+
